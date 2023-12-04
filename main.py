ca6554aaae822c4963d15760027fe13942fdc4ee
Dorkilla <dorkilla58@gmail.com>
2023-12-04 12:06:56 +0100

    orai

---
+++ main.py
@@ -0,0 +1,30 @@
+"""import Szamitogep
+
+szg1=Szamitogep.Szamitogep(0, False)
+szg2=Szamitogep.Szamitogep(0, False)
+
+szg1.kapcsol()
+if szg1.programMasol(800):
+    print("A másolás sikeres volt.")
+else:
+    print("A másolás sikertelen volt.")
+
+if szg1.programMasol(400):
+    print("A másolás sikeres volt.")
+else:
+    print("A másolás sikertelen volt.")
+
+if szg2.programMasol(1):
+    print("A másolás sikeres volt.")
+else:
+    print("A másolás sikertelen volt.")
+
+print(szg1)
+print(szg2)
+"""
+import elso
+#elso.elso()#masodik.masodik()
+import harmadik
+#harmadik.harmadik()
+import negyedik
+negyedik.negyedik()
